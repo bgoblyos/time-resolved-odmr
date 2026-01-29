1d9029bcdb64b4432cfe3b95470342c61e3f0646
Bence Göblyös <bence@goblyos.dev>
2026-01-29 14:21:34 +0100

    Major progress on SR830M driver

--- Devices/LockIn.py
+++ Devices/LockIn.py
@@ -87,19 +87,19 @@ class SR830M():
             data = [
                 [0,  6.25e-02, "62.5 mHz" ],
                 [1,  1.25e-01, "125 mHz"  ],
-                [2,  2.5e-01,  "250 mHz"  ],
-                [3,  5.0e-01,  "500 mHz"  ],
-                [4,  1.0e+00,  "1 Hz"     ],
-                [5,  2.0e+00,  "2 Hz"     ],
-                [6,  4.0e+00,  "4 Hz"     ],
-                [7,  8.0e+00,  "8 Hz"     ],
-                [8,  1.6e+01,  "16 Hz"    ],
-                [9,  3.2e+01,  "32 Hz"    ],
-                [10, 6.4e+01,  "64 Hz"    ],
+                [2,   2.5e-01, "250 mHz"  ],
+                [3,   5.0e-01, "500 mHz"  ],
+                [4,   1.0e+00, "1 Hz"     ],
+                [5,   2.0e+00, "2 Hz"     ],
+                [6,   4.0e+00, "4 Hz"     ],
+                [7,   8.0e+00, "8 Hz"     ],
+                [8,   1.6e+01, "16 Hz"    ],
+                [9,   3.2e+01, "32 Hz"    ],
+                [10,  6.4e+01, "64 Hz"    ],
                 [11, 1.28e+02, "128 Hz"   ],
                 [12, 2.56e+02, "256 Hz"   ],
                 [13, 5.12e+02, "512 Hz"   ],
-                [14, None,     "Trigger"  ]
+                [14,        0, "Trigger"  ]
             ]
         )
         
@@ -149,7 +149,326 @@ class SR830M():
              "D2": 11,
              "CH2": 11,
         }
+       
+    def setSensitivity(self, target, setMode = true):
+        """
+        Sets a specified sensitivity. 
+
+        Parameters
+        ----------
+        target: str or int
+            If str, try to parse it based on the translation table (see SR830M.sensDF).
+            If int, set it directly (see translation table or instrument manual). Negative values indicate current measurement mode.
         
+        setMode: Bool, default: True
+            Whether to automatically set the input mode. Defaults to A in voltage mode and I (100 MΩ) in current mode. Set to False for more granular control.
+
+        Returns
+        -------
+        sens: float
+            Achieved sensitivity (float). -1 indicates an error.
+        current: Bool
+            Voltage (False) or current (True) mode
+        """
+
+        i = None
+        current = False
+
+        if type(target) is str:
+            if target in self.sensDF.Vstr:
+                row = np.argwhere(self.sensDF.Vstr == target)[0,0]
+                i = self.sensDF.i[row]
+            elif target in self.sensDF.Istr:
+                row = np.argwhere(self.sensDF.Istr == target)[0,0]
+                i = self.sensDF.i[row]
+                current = True
+            else:
+                print("[SR830M] Requested sensitivity string is invalid.")
+                return -1, None
+
+        elif type(target) is int:
+            if target < 0:
+                target = -target
+                current = True
+
+            if target in self.sensDF.i:
+                i = target
+            else:
+                print("[SR830M] Requested sensitivity index is invalid.")
+                return -1, None
+        
+        else:
+            print("[SR830M] Requested sensitivity type is invalid.")
+            return -1, None
+
+        if current and setMode:
+            self.setInputMode(0)
+        elif setMode:
+            self.setInputMode(3)
+
+        self.device.write(f"SENS {i}")
+        
+        if current:
+            return self.sensDF.I[np.argwhere(self.sensDF.i == i)[0,0]]
+        else:
+            return self.sensDF.V[np.argwhere(self.sensDF.i == i)[0,0]]
+
+    def setSensitivityV(self, target, **kwargs):
+        row = np.argmin(np.abs(self.sensDF.V - target))
+        i = self.sensDF.i[row]
+        return self.setSens(i, **kwargs)
+
+    def setSensitivityA(self, target, **kwargs):
+        row = np.argmin(np.abs(self.sensDF.I - target))
+        i = self.sensDF.i[row]
+        return self.setSens(-i, **kwargs)
+    
+    def getSensitivity(self):
+        current = self.getInputMode() >= 2
+        i = int(self.query("SENS?")) 
+        row = np.argwhere(self.sensDF.i == i)[0,0]
+
+        if current:
+            return -i, np.sensDF.I[row]
+        else:
+            return i, np.sensDF.V[row]
+
+    def setSampleRate(self, target = None):
+        """
+        Sets a specified sample rate for automatic acquisition.
+
+        Parameters
+        ----------
+        target: None, str or int
+            Target sample rate. If None, set highest rate that is meaninful with the current time constant.
+            If str, try to parse it based on the translation table (see SR830M.srateDF).
+            If int, set it directly (see translation table or instrument manual).
+
+        Returns
+        -------
+        Achieved sample rate in Hz (float). Trigger mode corresponds to 0, while -1 indicates a failure.
+        """
+        if target is None:
+            # Attempt to set automatically based on time constant
+            _, t = self.getTau()
+            maxfreq = 1/t
+            candidates = self.srateDF.srate[self.srateDF.srate <= maxfreq]
+            maxvalid = np.max(candidates)
+            row = np.argwhere(self.srateDF.srate == maxvalid)[0,0]
+            i = self.srateDF.i[row]
+            self.device.write(f"SRAT {i}")
+            return maxvalid
+            
+        if type(target) is str:
+            res = np.argwhere(self.srateDF.sratestr == target)
+            if res.shape[0] < 1:
+                print("[SR830M] Requested sample rate string is invalid.")
+                return -1
+            else:
+                i = self.srateDF.i[res[0,0]]
+                self.device.write(f"SRAT {i}")
+                return self.srateDF.srate[res[0,0]]
+        
+        else:
+            print("[SR830M] Sample rate input type is invalid.")
+            return -1
+
+        elif type(target) is int:
+            if target in self.srateDF.i:
+                self.device.write(f"SRAT {target}")
+                return self.srateDF.srate[np.argwhere(self.srateDF.i == target)[0,0]]
+            else:
+                print("[SR830M] Requested sample rate index is invalid.")
+                return -1
+
+    def setSamplerateHz(self, target):
+        row = np.argmin(np.abs(self.srateDF.srate - target))
+        i = self.srateDF.i[row]
+        self.device.write(f"SRAT {i}")
+        return self.srateDF.srate[row]
+
+    def getSamplerate(self):
+        """
+        Query the device for the currently set sampling rate.
+
+        Returns
+        -------
+        (i, f): index and frequency in Hz
+        """
+        resp = int(self.query("SRAT?"))
+        i = np.argwhere(self.srateDF.i == resp)[0,0]
+        f = self.tauDF.srate[i]
+        return resp, f
+
+    def setTau(self, target):
+        """
+        Sets a specified time constant.
+
+        Parameters
+        ----------
+        target: str or int
+            If str, try to parse it based on the translation table (see SR830M.srateDF).
+            If int, set it directly (see translation table or instrument manual).
+
+        Returns
+        -------
+        Achieved time constant (float). -1 indicates an error.
+        """
+        if type(target) is str:
+            res = np.argwhere(self.tauDF.tsrt == target)
+            if res.shape[0] < 1:
+                print("[SR830M] Requested time constant string is invalid.")
+                return -1
+            else:
+                i = self.tauDF.i[res[0,0]]
+                self.device.write(f"OFLT {i}")
+                return self.tauDF.t[res[0,0]]
+
+        elif type(target) is int:
+            if target in self.tauDF.i:
+                self.device.write(f"OFLT {target}")
+                return self.tauDF.t[np.argwhere(self.tauDF.i == target)[0,0]]
+            else:
+                print("[SR830M] Requested time constant index is invalid.")
+                return -1
+        else:
+            print("[SR830M] Time constant input type is invalid.")
+            return -1
+
+    def setTauS(self, target):
+        row = np.argmin(np.abs(self.tauDF.t - target))
+        i = self.tauDF.i[row]
+        self.device.write(f"OFLT {i}")
+        return self.tauDF.t[row]
+
+    def getTau(self):
+        """
+        Query the device for the currently set time constant.
+
+        Returns
+        -------
+        (i, t): index and time in seconds
+        """
+        resp = int(self.query("OFLT?"))
+        i = np.argwhere(self.tauDF.i == resp)[0,0]
+        t = self.tauDF.t[i]
+        return resp, t
+
+
+    # Oscillator settings
+    def setLO(self, internal):
+        """
+        Set local oscillator source.
+
+        Parameters
+        ----------
+        internal: Bool
+            Set to True for internal, False for external source.
+        
+        """
+
+        if internal:
+            self.device.write("FMOD 1")
+        else:
+            self.device.write("FMOD 0")
+
+    def getLO(self):
+        """
+        Query which frequency source is in use.
+
+        Returns
+        -------
+        Bool: True for internal, False for external
+        """
+
+        resp = int(self.device.query("FMOD?"))
+        
+        return resp == 1
+
+    def setFreq(self, freq):
+        # TODO: Consider harmonic detection for bounds checking.
+        if freq >= 0.001 and freq <= 102000:
+            self.device.write(f"FREQ {freq}")
+            return True
+        else:
+            print("[SR830M] Requested LO frequency is out of bounds.")
+            return False
+
+    def getFreq(self):
+        return float(self.device.query("FREQ?"))
+
+    def setPhase(self, phase):
+        p = phase % 360 # It's easier to just wrap it here
+        self.device.write(f"PHAS {p}")
+
+    def getPhase(self):
+        return float(self.device.query("PHAS?"))
+
+    # Input configuration
+    def setInputMode(self, mode):
+        """
+        Sets the input mode of the device.
+
+        Parameters
+        ----------
+        mode: int
+            Possible values: 0 - A (voltage)
+                             1 - A-B (differential voltage)
+                             2 - I (1 MΩ)
+                             3 - I (100 MΩ)
+        
+        Returns
+        -------
+        success: Bool
+        """
+
+        if mode in [0, 1, 2, 3]:
+            self.device.write(f"ISRC {mode}")
+            return True
+        else:
+            print("[SR830M] Input mode must be one of [0, 1, 2, 3].")
+            return False
+
+    def getInputMode(self):
+        """
+        Gets the input mode of the device.
+
+        
+        Returns
+        -------
+        mode: int
+            Possible values: 0 - A (voltage)
+                             1 - A-B (differential voltage)
+                             2 - I (1 MΩ)
+                             3 - I (100 MΩ)
+        """
+        return int(self.device.query("ISRC?")) 
+
+    def setInputFloat(self, floating):
+        # TODO: Implement
+        return None
+
+    def getInputFloat(self):
+        # TODO: implement
+        return None
+
+    def setInputCoupling(self, dc):
+        # TODO: Implement
+        return None
+
+    def getInputCoupling(self):
+        # TODO: implement
+        return None
+
+    def setInputFilter(self, line, line2):
+        # TODO: Implement
+        return None
+
+    def getInputFilter(self):
+        # TODO: implement
+        return None
+
+    # Display settings
     def setDisplay(self, disp, target, ratio = 0):
         """
         Sets a specified display on the lock-in to a given value.
@@ -186,6 +505,10 @@ class SR830M():
             available = ", ".join(dispDict.keys())
             print(f"The requested value is invalid. Request: {target}. Available values: {available}")
             return False
+
+    def getDisplay(self):
+        #TODO: implement
+        return None
 
     def snapshot(self, params):
         if type(params) == str:
@@ -266,6 +589,3 @@ class SR830M():
 
        queryStr = f"TRCB ? {buffer}, {firstPoint}, {numPoints}"
        return self.queryBinaryFloat(queryStr)
-
-    # TODO: Implement function which takes in a desired sample rate,
-    # finds the closest achievable value, sets it and returns the result.
